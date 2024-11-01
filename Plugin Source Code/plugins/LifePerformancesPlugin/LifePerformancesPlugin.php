99a21e0acacd770895bde0f60273b98f01e853ad
Jacob Clark <clarkjac03@gmail.com>
2024-11-01 13:21:32 -0400

    Create Base for the plugin code

---
+++ Plugin Source Code/plugins/LifePerformancesPlugin/LifePerformancesPlugin.php
@@ -0,0 +1,8 @@
+<?php
+/** 
+ * Plugin Name: Life Performances Plugin
+ * Description: Plugin for the LtR Life Performances Page
+ * Authors:
+ * Version: 0.1.0
+ * Text Domain: life-performances-plugin
+*/
