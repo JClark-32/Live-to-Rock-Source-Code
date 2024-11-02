7ef98875548a562ab417b48e319d3e169a012c11
Jacob Clark <clarkjac03@gmail.com>
2024-11-02 17:20:46 -0400

    Add css file

--- Plugin Source Code/plugins/LifePerformancesPlugin/LifePerformancesPlugin.php
+++ Plugin Source Code/plugins/LifePerformancesPlugin/LifePerformancesPlugin.php
@@ -6,9 +6,31 @@
  * Version: 0.1.0
  * Text Domain: life-performances-plugin
 */
-if ( ! defined('ABSPATH') ) 
+if ( !defined('ABSPATH') ) 
 {
     exit;
 }
 
-echo "hello";
+class LifePerformances{
+
+    public function __construct(){
+
+    // Load assets
+    add_action('wp_enqueue_scripts', array( $this,'load_assets') );
+
+    }
+
+
+    public function load_assets(){
+        wp_enqueue_style(
+            'LifePerformancesPlugin', 
+            plugins_dir_url( __FILE__ ) . '/css/LifePerformancesPlugin.css',
+            array(),
+            1 ,
+            'all'
+        );
+    }
+
+}
+
+new LifePerformances();
