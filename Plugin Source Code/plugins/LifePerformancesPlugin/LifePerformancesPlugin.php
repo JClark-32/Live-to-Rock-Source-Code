962702eeb48b1f5f64b965b6dc8e8fe08a82eeed
Jacob Clark <clarkjac03@gmail.com>
2024-11-02 17:49:39 -0400

    Add shortcode call

--- Plugin Source Code/plugins/LifePerformancesPlugin/LifePerformancesPlugin.php
+++ Plugin Source Code/plugins/LifePerformancesPlugin/LifePerformancesPlugin.php
@@ -15,8 +15,11 @@ class LifePerformances{
 
     public function __construct(){
 
-    // Load assets
-    add_action('wp_enqueue_scripts', array( $this,'load_assets') );
+        // Load assets
+        add_action('wp_enqueue_scripts', array( $this,'load_assets') );
+
+        // Add shortcode
+        add_shortcode('life-performance', array( $this,'load_shortcode') );
 
     }
 
@@ -24,11 +27,24 @@ class LifePerformances{
     public function load_assets(){
         wp_enqueue_style(
             'LifePerformancesPlugin', 
-            plugins_dir_url( __FILE__ ) . '/css/LifePerformancesPlugin.css',
+            plugin_dir_url( __FILE__ ) . '/css/LifePerformancesPlugin.css',
             array(),
             1 ,
             'all'
         );
+
+        wp_enqueue_script(
+            'LifePerformancesPlugin',
+            plugin_dir_url( __FILE__ ) . '/js/LifePerformancesPlugin.js',
+            array(),
+            1 ,
+            'all'
+        );
+    }
+
+    public function load_shortcode()
+    {
+        return 'Hello, the shortcode is working';
     }
 
 }
