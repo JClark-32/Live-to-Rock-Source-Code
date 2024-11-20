57c05fc568a90ef9e084af0963b57aca25c9a6cb
Jacob Clark <clarkjac03@gmail.com>
2024-11-20 15:09:34 -0500

    Create base method of plugin

---
+++ Plugin Source Code/plugins/JamSessionPlugin/JamSessionPlugin.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: Jam Session Plugin
+ * Description: Adds blog posts
+ * Authors: Live to Rock Capstone Team
+ * Version: 0.1.0
+ * Text Domain: jam-session-plugin
+ */
+
+ // Prevent public user access to .php file
+ if ( !defined('ABSPATH') ) { exit; }
+
+ Class JamSession{
+
+ }
+ new JamSession();
