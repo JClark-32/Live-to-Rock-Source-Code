6557600ef09c4b543386d040b76b257670156d97
cealcorn <cassidyealcorn@gmail.com>
2024-12-02 21:41:02 -0500

    successful test! (risky bc of lack of code coverage?)

--- tests/Unit/LifePerformancesTest.php
+++ tests/Unit/LifePerformancesTest.php
@@ -4,20 +4,49 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 
 class LifePerformancesTest extends TestCase
 {
+    protected function setUp(): void
+    {
+        // define ABSPATH to prevent class from immediately exiting
+        if (!defined('ABSPATH')) {
+            define('ABSPATH', __DIR__ . '/');
+        }
+
+        // other mocked functions?
+        if (!function_exists('add_action')) {
+            $mocked_actions = [];
+            function add_action($hook, $callback)
+            {
+                global $mocked_actions;
+                $mocked_actions[] = [
+                    'hook' => $hook,
+                    'callback' => $callback
+                ];
+            }
+        }
+        if (!function_exists('add_shortcode')) {
+            function add_shortcode($tag, $callback)
+            {
+                global $mocked_shortcodes;
+                $mocked_shortcodes[$tag] = $callback;
+            }
+        }
+    }
+
     public function testLoadVideoSubmission()
     {
-        echo "Starting test...\n";
+        // echo "Starting test...\n";
         // $this->assertTrue(class_exists('LifePerformances'), "The class 'LifePerformances' does not exist.");
+        // echo "Class exists.";
 
         try {
             $lifePerformance = new LifePerformances();
-            echo "Class instantiated...\n";
+            // echo "Class instantiated...\n";
         } catch (Exception $e) {
             echo "Exception caught: " . $e->getMessage() . "\n";
         }
 
         $output = $lifePerformance->load_video_submission();
-        echo "Method executed...\n";
+        // echo "Method executed...\n";
 
         $expected = '<div id="ltr-video-submission">
             <h2>Post Your Life Performance?</h2>
