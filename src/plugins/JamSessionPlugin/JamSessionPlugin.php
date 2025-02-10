666a9059a991128892e05685cc5695bfaa8eb4db
Jacob Clark <clarkjac03@gmail.com>
2025-02-10 14:00:18 -0500

    Separate table creation into new file

--- src/plugins/JamSessionPlugin/JamSessionPlugin.php
+++ src/plugins/JamSessionPlugin/JamSessionPlugin.php
@@ -71,43 +71,7 @@
     }
 
     function create_db_tables($table_name){
-        $sql = "CREATE TABLE IF NOT EXISTS $table_name(\n"
-        . "    id INT(9) NOT NULL AUTO_INCREMENT,\n"
-        . "    user_posted VARCHAR(60) NOT NULL,\n"
-        . "    date_posted DATETIME DEFAULT CURRENT_TIMESTAMP,\n"
-        . "    blog_title TEXT NOT NULL,\n"
-        . "    blog_text TEXT NOT NULL,\n"
-        . "    blog_author TEXT NOT NULL,\n"
-        . "    PRIMARY KEY(id)"
-        . ");";
-
-        $likes_table_name = $wpdb->prefix . 'blog_post_likes';
-
-        $likes_sql = "CREATE TABLE IF NOT EXISTS $likes_table_name(\n"
-        . "    id INT(9) NOT NULL AUTO_INCREMENT,\n"
-        . "    user_liked VARCHAR(60) NOT NULL,\n"
-        . "    blog_id INT(9),\n"
-        . "    PRIMARY KEY(id),\n"
-        . "    FOREIGN KEY(blog_id) REFERENCES $table_name(id)"
-        . ");";
-
-        $comments_table_name = $wpdb->prefix . 'blog_post_comments';
-
-        $comments_sql = "CREATE TABLE IF NOT EXISTS $comments_table_name(\n"
-        . "    id INT(9) NOT NULL AUTO_INCREMENT,\n"
-        . "    user_commented VARCHAR(60) NOT NULL,\n"
-        . "    date_posted DATETIME DEFAULT CURRENT_TIMESTAMP,\n"
-        . "    blog_id INT(9),\n"
-        . "    comment_text TEXT,\n"
-        . "    PRIMARY KEY(id),\n"
-        . "    FOREIGN KEY(blog_id) REFERENCES $table_name(id)"
-        . ");";
-
-        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-        
-        dbDelta($sql);
-        dbDelta($likes_sql);
-        dbDelta($comments_sql);
+        include 'databaseTableCreation.php';
     }
 
     public function blog_id(){
@@ -126,6 +90,7 @@
             } else {
                 echo "Error";
             }
+            
 
             $table_name = $wpdb->prefix . 'blog_post';
             $this->create_db_tables($table_name);
@@ -198,7 +163,7 @@
 
         ?>
         <script>
-            //Transfers the entries into arrays
+            //Transfers the entries into javascript arrays
             blogIds = <?php echo json_encode($blog_ids) ?> ;
             blogTexts = <?php echo json_encode($blog_texts) ?> ;
             blogTitles = <?php echo json_encode($blog_titles) ?> ;
