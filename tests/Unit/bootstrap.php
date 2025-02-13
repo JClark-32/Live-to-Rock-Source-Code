d35ff0a3c9cfc8a9c940c2bd20094fe92d6f3767
Cass <89551610+cealcorn@users.noreply.github.com>
2025-02-13 14:40:03 -0500

    update phpunit.xml + create bootstrap file for tests

---
+++ tests/Unit/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Brain\Monkey;
+
+// Set up Brain Monkey before each test
+Brain\Monkey\setUp();
+
+// Tear down Brain Monkey after each test
+Brain\Monkey\tearDown();
