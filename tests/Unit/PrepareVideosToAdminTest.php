0b335027d4acdc50067dde09b00d38d351e48302
Cass <89551610+cealcorn@users.noreply.github.com>
2025-03-13 15:05:03 -0400

    test class templates created for LP class

---
+++ tests/Unit/PrepareVideosToAdminTest.php
@@ -0,0 +1,4 @@
+<?php
+use PHPUnit\Framework\TestCase;
+require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../../src/plugins/LifePerformancesPlugin/PrepareVideosToAdmin.php';
