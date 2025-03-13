0668715703bceafeda9100ebe665a78b732f6548
Cass <89551610+cealcorn@users.noreply.github.com>
2025-03-13 15:12:57 -0400

    added class

--- tests/Unit/CheckSubmitButtonTest.php
+++ tests/Unit/CheckSubmitButtonTest.php
@@ -3,3 +3,5 @@
 use PHPUnit\Framework\TestCase;
 require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/../../src/plugins/LifePerformancesPlugin/CheckSubmitButton.php';
+
+Class CheckSubmitButtonTest extends TestCase {}
--- tests/Unit/PrepareVideosToAdminTest.php
+++ tests/Unit/PrepareVideosToAdminTest.php
@@ -2,3 +2,5 @@
 use PHPUnit\Framework\TestCase;
 require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/../../src/plugins/LifePerformancesPlugin/PrepareVideosToAdmin.php';
+
+Class PrepareVideosToAdminTest extends TestCase {}
