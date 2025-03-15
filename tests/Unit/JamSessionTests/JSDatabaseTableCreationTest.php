bca2a2a1561e8705d2c9c33d30c3714b80e0ac03
Cass <89551610+cealcorn@users.noreply.github.com>
2025-03-14 20:09:09 -0400

    test template

---
+++ tests/Unit/JamSessionTests/JSDatabaseTableCreationTest.php
@@ -0,0 +1,6 @@
+<?php
+use PHPUnit\Framework\TestCase;
+require_once __DIR__ . '/../../../vendor/autoload.php';
+require_once __DIR__ . '/../../../src/plugins/JamSessionPlugin/includes/JSDatabaseTableCreation.php';
+
+Class JSDatabaseTableCreationTest extends TestCase {}
