b09c98c423a09c0a7fadb334a742df6e2be02a07
Cass <89551610+cealcorn@users.noreply.github.com>
2025-03-17 15:49:32 -0400

    Create CreateVideosSpaceTest.php

---
+++ tests/Unit/LifePerformancesTests/CreateVideosSpaceTest.php
@@ -0,0 +1,9 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+require_once __DIR__ . '/../../../vendor/autoload.php';
+require_once __DIR__ . '/../../../src/plugins/LifePerformancesPlugin/includes/CreateVideosSpace.php';
+
+Class CreateVideosSpaceTest extends TestCase {
+    
+}
