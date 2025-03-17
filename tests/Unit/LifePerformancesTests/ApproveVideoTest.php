6bdcdf3536e8a60297ff99aa41dc73239ba89988
Cass <89551610+cealcorn@users.noreply.github.com>
2025-03-17 15:44:22 -0400

    test written & working
    PHPUNIT as a constant prevents the test from exiting. This was written into the main class in the form of an if statement that the code will essentially bypass UNLESS its being run as a test in local

--- tests/Unit/LifePerformancesTests/ApproveVideoTest.php
+++ tests/Unit/LifePerformancesTests/ApproveVideoTest.php
@@ -17,5 +17,51 @@ if (!function_exists('get_site_url')) {
 require_once __DIR__ . '/../../../src/plugins/LifePerformancesPlugin/includes/ApproveVideo.php';
 
 class ApproveVideoTest extends TestCase {
-    
+    protected $logFile;
+
+    protected function setUp(): void {
+        parent::setUp();
+
+        $GLOBALS['wp_redirect'] = null;
+         
+        global $wpdb;
+        $wpdb = $this->getMockBuilder(stdClass::class)
+            ->addMethods(['update'])
+            ->getMock();
+        $wpdb->prefix = 'wp_';
+        $wpdb->last_error = '';
+
+        $wpdb->method('update')->willReturn(1); // one row updated
+    }
+
+    protected function tearDown(): void {
+        global $wpdb;
+        $wpdb = null; // reset
+
+        parent::tearDown();
+    }
+
+    public function testApproveVideo() {
+        global $wpdb;
+
+        $wpdb->expects($this->once())
+             ->method('update')
+             ->with(
+                 'wp_video_submission',
+                 ['approved' => 1],
+                 ['submission_text' => 'dQw4w9WgXcQ'],
+                 ['%d'],
+                 ['%s']
+             )
+             ->willReturn(1);
+
+        // define PHPUNIT_RUNNING so test doesn't exit
+        if (!defined('PHPUNIT_RUNNING')) {
+            define('PHPUNIT_RUNNING', true);
+        }
+
+        approve_video('dQw4w9WgXcQ');
+
+        $this->assertEquals('http://example.com/life-performances', $GLOBALS['wp_redirect']);
+    }
 }
