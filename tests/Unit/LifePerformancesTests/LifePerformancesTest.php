57fb72f8b736283b284168be6bb93102db2c90dc
Cass <89551610+cealcorn@users.noreply.github.com>
2025-03-17 14:43:08 -0400

    removed duplicated test

--- tests/Unit/LifePerformancesTests/LifePerformancesTest.php
+++ tests/Unit/LifePerformancesTests/LifePerformancesTest.php
@@ -114,40 +114,4 @@ class LifePerformancesTest extends TestCase
         $this->assertSame('', $output, 'Expected blank_shortcode to return an empty string.');
     }
 
-    // LOAD VIDEO SUBMISSIONS
-    public function testLoadVideoSubmission()
-    {
-        // echo "Starting test...\n";
-        // $this->assertTrue(class_exists('LifePerformances'), "The class 'LifePerformances' does not exist.");
-        // echo "Class exists.";
-
-        try {
-            $lifePerformance = new LifePerformances();
-            // echo "Class instantiated...\n";
-        } catch (Exception $e) {
-            echo "Exception caught: " . $e->getMessage() . "\n";
-        }
-
-        $output = $lifePerformance->load_video_submission();
-        // echo "Method executed...\n";
-
-        $expected = '<div id="ltr-video-submission">
-            <h2>Post Your Life Performance?</h2>
-            <p>Paste YouTube URL here:</p>
-            <form id="ltr-video-link" method="post">
-                <div class="input">
-                    <input type="url" name="ltr-video-url" placeholder="YouTube URL" required>
-                </div>
-                <div id="ltr-submit">
-                    <button type="submit" name="ltr-submit-video-button" class="submit-btn">Submit!</button>
-                </div>
-            </form>
-        </div>';
-
-        $expected = preg_replace('/\s+/', '', $expected);
-        $output = preg_replace('/\s+/', '', $output);
-
-        $this->assertSame($expected, $output, "The output HTML does not match the expected structure.");
-    }
-
 }
