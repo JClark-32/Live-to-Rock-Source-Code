2f61b66445e73a3f428dc3bdde5260542b1aaad7
Jacob Clark <clarkjac03@gmail.com>
2025-04-07 13:03:53 -0400

    Create Podcast Plugin Files

---
+++ src/plugins/LtRPodcastsPlugin/LtRPodcastsPlugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: LtR Podcasts Plugin
+ * Description: Adds podcast playlist integration
+ * Authors: Live to Rock Capstone Team
+ * Version: 0.1.0
+ * Text Domain: ltr-podcast-plugin
+ */
+
+// Prevent public user access to .php file
+if ( !defined('ABSPATH') ) { exit; }
+
+Class LtRPodcasts{
+
+}
