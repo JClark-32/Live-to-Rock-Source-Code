5efaf001fc5df4cd02ecf6903d85da7603c40ca6
Jacob Clark <clarkjac03@gmail.com>
2025-04-15 02:26:49 -0400

    Store value from the playlist text entry field

--- src/plugins/LtRPodcastsPlugin/LtRPodcastsPlugin.php
+++ src/plugins/LtRPodcastsPlugin/LtRPodcastsPlugin.php
@@ -13,7 +13,8 @@ if ( !defined('ABSPATH') ) { exit; }
 Class LtRPodcasts{
     public function __construct(){
         add_action( 'admin_menu', array($this, 'wporg_options_page'));
-        //add_action('init', array( $this,'createAdminMenu') );
+        add_action('init', array( $this,'podcast_id') );
+        add_action('admin_post_save_ltr_playlist', array($this, 'save_playlist_url'));
         add_shortcode('ltr-podcasts', array ( $this, 'podcast_shortcode') );
     }
 
@@ -21,14 +22,35 @@ Class LtRPodcasts{
     function wporg_options_page_html() {
         ?>
         <div class="wrap">
-          <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
-          <form action="options.php" method="post">
-            <textarea>Enter Playlist Url</textarea>
-            <button style="submit">Change</button>
-          </form>
+            <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+            <form action="<?php echo admin_url('admin-post.php'); ?>" method="post">
+                <textarea name="ltr-playlist-url" rows="5" cols="40"><?php echo esc_textarea(get_option('ltr_playlist_url', '')); ?></textarea>
+                <input type="hidden" name="action" value="save_ltr_playlist">
+                <?php submit_button('Save Playlist URL'); ?>
+            </form>
         </div>
         <?php
     }
+
+    public function save_playlist_url() {
+        if (current_user_can('manage_options') && isset($_POST['ltr-playlist-url'])) {
+            $playlist = sanitize_text_field($_POST['ltr-playlist-url']);
+            update_option('ltr_playlist_url', $playlist);
+        }
+    
+        // Redirect back to the settings page
+        wp_redirect(admin_url('admin.php?page=ltr_podcasts&updated=true'));
+        exit;
+    }
+    
+    public function podcast_id() {
+        if (isset($_POST['ltr-playlist-url'])) {
+            // Sanitize and save the value
+            $playlist = sanitize_text_field($_POST['ltr-playlist-url']);
+            update_option('ltr_playlist_url', $playlist);
+        }
+    }
+    
     
     function wporg_options_page() {
         add_menu_page(
@@ -43,8 +65,12 @@ Class LtRPodcasts{
     }
 
     public function podcast_shortcode() {
+        $playlist = get_option('ltr_playlist_url', '');
         ob_start();
         ?>
+        <p>
+            <?php echo esc_html($playlist); ?>
+        </p>
         <?php
         return ob_get_clean();
     }
