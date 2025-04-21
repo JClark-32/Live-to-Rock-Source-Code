7ad5630354dc581a55089c2543ff34e5c17a01cf
Cass <89551610+cealcorn@users.noreply.github.com>
2025-04-20 21:33:06 -0400

    d

--- tests/Unit/JamSessionTests/CheckIfUserLikedTest.php
+++ tests/Unit/JamSessionTests/CheckIfUserLikedTest.php
@@ -4,7 +4,7 @@ use PHPUnit\Framework\TestCase;
 require_once __DIR__ . '/../../../vendor/autoload.php';
 
 // WORDPRESS MOCKS
-require_once __DIR__ . '/Bootstrap.php';
+require_once __DIR__ . '/bootstrap.php';
 
 require_once __DIR__ . '/../../../src/plugins/JamSessionPlugin/includes/JSDatabaseCalls.php';
 
